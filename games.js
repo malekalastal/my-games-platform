d442ac8d5e74e07decd2fd18ba099b9fc8b8283b
Malek AL-astal <malekalastal111@gmail.com>
2025-09-20 11:09:33 +0300

    Create games.js

---
+++ games.js
@@ -0,0 +1,12 @@
+const games = [
+  {
+    title: "لعبة 1",
+    image: "images/game1.jpg",
+    url: "games/game1/index.html"
+  },
+  {
+    title: "لعبة 2",
+    image: "images/game2.jpg",
+    url: "games/game2/index.html"
+  }
+];
